d1ca61a8d75246238178a871b8cc38e4e92879eb
ViktoriyaTru <viktoriya.trubitsina@gmail.com>
2024-03-19 20:21:24 +0300

    add lesson3:Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿double calculateformula(int a, int b, int c, int d)
+{
+    double numinator = a * b;
+    int denominator = c + d;
+    double result = numinator / denominator;
+    return result;
+}
+
+double result = calculateformula(1, 2, 3, 4);
+
+System.Console.WriteLine(result);
