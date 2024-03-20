422111b4579eb8dd354a7878fc7c1ba76fe7de69
ViktoriyaTru <viktoriya.trubitsina@gmail.com>
2024-03-20 12:41:06 +0300

    add lesson3:Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿void ZeroEvenElements(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+void PrintArray(int[] arr)
+{
+    foreach (int e in arr)
+    {
+        System.Console.WriteLine($"{e} ");
+    }
+}
+
+int[] array = { 20, 2, 0, 4, 7 };
+
+ZeroEvenElements(array);
+PrintArray(array);
